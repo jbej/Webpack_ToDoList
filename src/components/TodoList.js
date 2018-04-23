36f9842a34f24cca4fa00512cb59e081ed6abc2d
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-04-23 20:19:26 +0300

    Change lines in index.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,6 @@ import style from './TodoList.css';
 import Todo from './Todo';
 import TodoForm from './TodoForm';
 
-
-
 const TodoList = props => {
 	const todoListItems = props.data.map(item => <Todo key={item.id} id={item.id} todo={item.text}
 		remove={props.remove} iform={props.iform}/>)
