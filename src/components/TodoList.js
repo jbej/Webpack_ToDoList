50634d62b69ae847f1275a7104ad7a2bcfbf1c3b
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-04-24 09:47:28 +0300

    Modified src/components/TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ import TodoForm from './TodoForm';
 
 const TodoList = props => {
 	const todoListItems = props.data.map(item => <Todo key={item.id} id={item.id} todo={item.text}
-		remove={props.remove} iform={props.iform}/>)
+		remove={props.remove} iform={props.iform}/>);
 	return(
 		<div className={style.TodoList}>{todoListItems}
 		</div>
